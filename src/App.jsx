0b32ed623ee4e1e0d15e98e8d824eb763dde01f0
yoshidont-mind <tyoshida6@my.bcit.ca>
2024-03-09 15:39:52 -0800

    Add congratulations to app

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home.jsx';
 import Timer from './pages/Timer.jsx';
 import SetDailyGoal from './pages/SetDailyGoal.jsx';
 import TaskList from './components/TaskList/TaskList.jsx';
+import Congratulations from 'components/Congratulations/Congratulations.jsx';
 
 const router = createBrowserRouter([
   {
@@ -21,6 +22,10 @@ const router = createBrowserRouter([
     path: '/task-card',
     element: <TaskList />,
   },
+  {
+    path: '/congratulations',
+    element: <Congratulations />,
+  }
 ]);
 
 function App() {
