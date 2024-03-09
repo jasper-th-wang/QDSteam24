da399405b642c2b46b6946ab21a86ff8d8bdb5c7
Jasper Wang <jasper8777@icloud.com>
2024-03-09 13:07:26 -0800

    add files

---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,35 @@
+import Paper from '@mui/material/Paper';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import { useState } from 'react';
+import classes from './Container.module.css';
+function Container({ children }) {
+  const [value, setValue] = useState(0);
+
+  return (
+    <div>
+      <div className={classes.container}>{children}</div>
+      <Paper
+        sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }}
+        elevation={3}
+      >
+        <BottomNavigation
+          showLabels
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        >
+          <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+          <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+          <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+        </BottomNavigation>
+      </Paper>
+    </div>
+  );
+}
+
+export default Container;
