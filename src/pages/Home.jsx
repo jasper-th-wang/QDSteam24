f9ae011f3cac361803589c6cd9a27ab7add1e9a9
Jasper Wang <jasper8777@icloud.com>
2024-03-09 11:03:54 -0800

    fix bugs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
-import { Button } from '@mui/base/Button';
+import Button from '@mui/material/Button';
 function Home({ someNumber }) {
   return (
     <div className="">
       <p>Hello QDS24 GAHAHHAHAHHA</p>
       <p>{someNumber}</p>
-      <Button />
+      <Button variant="contained">Hello world</Button>
     </div>
   );
 }
