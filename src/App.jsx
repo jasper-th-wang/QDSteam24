6b1256a0847eff4fe65fb066029c94570fc93812
Misuzu <me.tpn23@gmail.com>
2024-03-09 11:17:41 -0800

    Add button

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
     <>
       <div>
         <Home someNumber={5} />
+        <TimeGoal />
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
