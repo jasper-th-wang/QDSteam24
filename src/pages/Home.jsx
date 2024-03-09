65f090a9b9607e9538c0775966267c76f2522aa5
Misuzu <me.tpn23@gmail.com>
2024-03-09 15:08:26 -0800

    Implement TaskList in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,16 @@
+import { Task } from '@mui/icons-material';
 import Container from '../components/Container/Container';
 // import Button from '@mui/material/Button';
 import DailyProgress from '../components/DailyProgress/DailyProgress';
 import Pet from '../components/Pet/Pet';
+import TaskList from '../components/TaskList/TaskList';
 
 function Home() {
   return (
     <Container>
       <DailyProgress />
       <Pet mode={1} />
-      <h3>Today's Tasks:</h3>
-      <ul>
-        <li>Task item</li>
-        <li>Task item</li>
-        <li>Task item</li>
-      </ul>
+      <TaskList />
     </Container>
   );
 }
